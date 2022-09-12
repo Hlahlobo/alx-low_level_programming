efced03b54cdb409931668d2a6ebdc861418012f
Hlahlobo <hfrakuoane@gmail.com>
2022-09-12 10:00:35 -0700

    bases

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Print all base 10 numbers
+ *
+ * Description: all base 10 numbers starting from 0
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%i", i);
+	}
+	printf("\n");
+	return (0);
+
+}
