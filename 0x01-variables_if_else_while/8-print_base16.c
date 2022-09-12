8cc715328fa493ffb03582264ff70223e280c67c
Hlahlobo <hfrakuoane@gmail.com>
2022-09-12 10:45:03 -0700

    done

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,3 +1,24 @@
 #include <stdio.h>
 /**
- * main - A 
+ * main - A program toprint Hexadecimals
+ *
+ * Description:It dispalys 0-9 then A-Z
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char c;
+	char cn;
+
+	for (cn = '0'; cn <= 9; cn++)
+	{
+		putchar(cn);
+	}
+	for (cn = 'A'; cn <= 'Z'; cn++)
+	{
+		putchar(cn);
+	}
+	putchar('\n');
+	return (0);
+}
