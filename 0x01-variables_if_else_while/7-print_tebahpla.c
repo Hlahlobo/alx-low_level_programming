035f41e1b8b9cd8e6ed598e80bfc86f2853c51e0
Hlahlobo <hfrakuoane@gmail.com>
2022-09-12 10:21:38 -0700

    alphaz

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - A programme that displays ztoa
+ *
+ * Discription: Displays all alphabets in reverse
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'z'; ch >= 'z'; ch--)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+
+}
