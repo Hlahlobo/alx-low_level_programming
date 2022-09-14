b1bce83edfee81c0966a425e94f85986e3fc514b
Hlahlobo <hfrakuoane@gmail.com>
2022-09-14 14:09:39 -0700

    10-add.c

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
  * add - a function that adds two integers and returns the result
- * @num1 -first integer added
- * @num2 - second interger added
+ * @num1: first integer added
+ * @num2:  second integer added
  *
  * Return: 0
  */
