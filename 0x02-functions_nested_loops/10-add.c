e84542f7653847f40e5dd94fdad0acb09c8ee828
Hlahlobo <hfrakuoane@gmail.com>
2022-09-14 14:05:35 -0700

    vim 10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * add - a function that adds two integers and returns the result
+ * @num1 -first integer added
+ * @num2 - second interger added
+ *
+ * Return: 0
+ */
+
+int add(int num1, int num2)
+{
+	return (num1 + num2);
+}
