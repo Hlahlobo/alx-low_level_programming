42dd03a3dd95b50230be882c9630c8c10a842a2b
Hlahlobo <hfrakuoane@gmail.com>
2022-09-16 06:07:40 -0700

    a function that checks for uppercase character

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isupper - checks if the charecter is upper or not 
+ * @c: charecter to be tested
+ * Return: 1 when if higher, othewise 0
+ */
+
+int _isupper(int c)
+{
+	if ((c >= 'A') && (c <= 'Z'))
+	{
+		return (1);
+	}
+	return (0);
+}
---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,22 @@
+#ifndef MAIN_H_INCLUDED
+
+#define MAIN_H_INCLUED
+
+/**
+ * main - header file for prototypes
+ * void prototype (void)
+ * int prototype (int)
+ */
+
+void print_numbers(void);
+void print_most_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
+int _putchar(int);
+int _isupper(int);
+int _isdigit(int);
+int mult(int, int);
+
+#endif /* MAIN_H */
