c88dce775d04fbdc64ee8559393fb2edc8337a6e
Hlahlobo <hfrakuoane@gmail.com>
2022-09-16 12:42:20 -0700

    done

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -3,7 +3,7 @@
 
 /**
  *print_triangle - function to make triangle
- *
+ *@size: parameter
  * Return: nothing
  *
  */
@@ -16,7 +16,7 @@ void print_triangle(int size)
 	{
 		for (inc1 = 1; inc1 <= size; inc1++)
 		{
-			for ((inc2 = size -inc1); inc2 > 0; inc2--)
+			for ((inc2 = size - inc1); inc2 > 0; inc2--)
 			{
 				_putchar(' ');
 			}
