61b522126becddb5f2e04f215caa5b0ca035fdc9
Hlahlobo <hfrakuoane@gmail.com>
2022-09-19 07:21:18 -0700

    1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * swap_int - Afuntion to swap 2 numbers
+ * @a: integer one
+ * @b: integer two
+ * Return: Nothing
+ */
+
+void swap_int(int *a, int *b)
+{
+	int temp = *a;
+
+	*a = *b;
+
+	b = temp;
+}
