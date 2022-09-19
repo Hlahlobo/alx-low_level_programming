e95b27f5ec3c3deb4f8f92590d44f7da31b39be6
Hlahlobo <hfrakuoane@gmail.com>
2022-09-19 08:34:41 -0700

    3-puts.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -4,11 +4,11 @@
 
 /**
  *print_rev - Afunction to preview string
- *@s - string reverse
- *return -nothing
+ *@s: string reverse
+ *return: nothing
  */
 
- void print_rev(char *s)
+void print_rev(char *s)
 {
 	int len = strlen(s);
 
---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *void rev_string(char *s)
+ *
+ */
+
+void rev_string(char *s)
+{
+	
+}
