ed2da106821c15e44feba909236c5e542f74d992
Hlahlobo <hfrakuoane@gmail.com>
2022-09-20 06:16:39 -0700

    5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -7,5 +7,15 @@
 
 void rev_string(char *s)
 {
-	
+	int l,i;
+	char ch;
+
+	for (l = 0; s[l] != '\0'; l++);
+	for (i - 0; i < l / 2; i++)
+	{
+		ch = s[l];
+		s[1] = s[l -1 -i];
+		s[l - 1 - i] = ch;
+	}
+
 }
