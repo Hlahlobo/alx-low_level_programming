98d8c24a6657585a57ba6355163cafa4a79776d0
Hlahlobo <hfrakuoane@gmail.com>
2022-09-21 10:15:21 -0700

    1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -3,7 +3,7 @@
  * _strncat - a function that concatenates two strings.
  * @dest: buffer storing the string copy
  * @src: the source string
- * @max: max number
+ * @n: parameter
  * Return: returns
  */
 
@@ -11,10 +11,10 @@ char *_strncat(char *dest, char *src, int n)
 {
 	int i;
 
-	for (i = 0; i < n && src[i] != '\0'; int n)
-	 dest[i] = src[i];
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
 
-	for ( ; i <n; i++)
+	for ( ; i < n; i++)
 		dest[i] = '\0';
 
 	return (dest);
