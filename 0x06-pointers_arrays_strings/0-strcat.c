90d3095cc57924526481e06e32e5ea34c99a4c19
Hlahlobo <hfrakuoane@gmail.com>
2022-09-21 09:22:54 -0700

    char *_strncat(char *dest, char *src, int n)

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strncat -  concatenates two strings
+ * @dest: String that will append
+ * @src: String too be concentrated
+ *
+ * Return: returns pointer @dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int index = 0, dest_len = 0;
+
+	while (dest[index++])
+		dest_len++;
+
+	for (index = 0; src[index]; index++)
+		dest[dest_len++] = src[index];
+
+	return (dest);
+}
---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,3 @@
+Read me File
+for 0x06-pointers_arrays_strings
+More pointers and arrays
