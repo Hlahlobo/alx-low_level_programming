2640679e7f45a348a85e17092f4030511375bf75
Hlahlobo <hfrakuoane@gmail.com>
2022-09-22 10:46:17 -0700

    1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -4,18 +4,19 @@
  * @dest: buffer storing the string copy
  * @src: the source string
  * @n: parameter
- * Return: returns
+ * Return: dest
  */
 
 char *_strncat(char *dest, char *src, int n)
 {
-int i;
 
-for (i = 0; i < n && src[i] != '\0'; i++)
-	dest[i] = src[i];
+int index = 0, dest_len = 0;
 
-for ( ; i < n; i++)
-	dest[i] = '\0';
+while (dest[index++])
+	dest_len++;
+for (index = 0; src[index] && index < n;)
+	dest[dest_len++] = src[index];
 
 return (dest);
+
 }
