534c9c56a0a34f1234d34f74d4af6104ca04f8c7
Hlahlobo <hfrakuoane@gmail.com>
2022-09-27 07:33:11 -0700

    0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -5,14 +5,15 @@
  * _memset - function fills the first n bytes of the memory area pointed
  * @s: address to memory block
  * @b: char to be used
- * @n: 
+ * @n: number of bytes used
+ * Return: pointer
  */
 
 char *_memset(char *s, char b, unsigned int n)
 {
 	while (n)
 	{
-		s[n - 1]  = b;
+		s[n - 1] = b;
 		n--;
 	}
 	return (s);
