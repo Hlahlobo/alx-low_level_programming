2d5af1cdeb6a6d6972dd2e7bb4017bccd89e1090
Hlahlobo <hfrakuoane@gmail.com>
2022-09-28 09:18:41 -0700

    3-factorial.c

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -9,14 +9,13 @@
 
 int factorial(int n)
 {
-	int result = n;
+	if (n == 1)
+		return (1);
 
-	if (n > 0)
+	else if (n < 0)
 		return (-1);
 
-	else if (n >= 0 && n <= 1)
-		return (1);
 	else
-		return (result);
+		return (n * factorial(n - 1));
 }
 
