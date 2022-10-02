56c5ae565bb788277fb5a953e0c2e2ee1dff3252
Hlahlobo <hfrakuoane@gmail.com>
2022-10-02 07:52:05 -0700

    0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,14 +2,16 @@
 
 /**
  * main - Function that prints out the name of a program
- * @_attribute_((unused): Argument count
+ * @argc: Argument count
  * @argv: Argument strings
  *
  * Return: Always 0
  */
 
-int main(int argc _attribute_((unused)), char *argv[])
+int main(int argc, char *argv[])
 {
+	(void) argc;
+
 	printf("%s\n", argv[0]);
 	return (0);
 }
