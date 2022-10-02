b0d78e7532aa54c35755f49ed2881f2acc430418
Hlahlobo <hfrakuoane@gmail.com>
2022-10-02 08:29:24 -0700

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - a function that prints the number of arguments passed into it
+ * @argc: argument count
+ * @argv: arguments
+ *
+ * Return: Always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	void(argv);
+
+	printf("%i\n", argc - 1);
+
+	return (0);
+}
