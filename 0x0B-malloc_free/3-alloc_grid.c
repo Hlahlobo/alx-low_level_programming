dcdd367392afda19c4f50b5e8167de608135edbd
Hlahlobo <hfrakuoane@gmail.com>
2022-10-05 08:39:30 -0700

    int **alloc_grid(int width, int height)

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -10,13 +10,37 @@
  *Return: nothing
  */
 
-void free_grid(int **grid, int height)
+int **alloc_grid(int width, int height)
 {
+	int **grid;
 	int i;
+	int j;
+if (width <= 0 || height <= 0)
+{
+	return (NULL);
+}
 
-	for (i = 0; i < height; i++)
+grid = malloc(sizeof(int *) * height);
+
+if (grid == NULL)
+	return (NULL);
+
+for (i = 0; i < height; i++)
+{
+	grid[i] = malloc(sizeof(int) *width);
+
+	if (grid == NULL)
 	{
-		free(grid[i]);
+		for (i = i - 1; i >= 0; i--)
+		{
+			free(grid[i]);
+		}
+		free(grid);
+		return (NULL);
 	}
-	free(grid[i]);
+}
+
+for (i = 0; j < width; j++)
+grid[i][j] = 0;
+return (grid);
 }
