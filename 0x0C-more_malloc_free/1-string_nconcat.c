38a68edbb676114b38ee5e0109bd82bddf696cc0
Hlahlobo <hfrakuoane@gmail.com>
2022-10-07 06:31:14 -0700

    1-string_nconcat.c

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,63 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _strlen - Function to test string Lengh
+ * @string: String
+ *
+ * Return: String Length
+ */
+
+int _strlen(char *string)
+{
+	int i;
+
+	for (i = 0; string[i] != '\0'; i++)
+	{
+		return (i);
+	}
+}
+
+
+/**
+ * string_nconcat - a function that concatenates two strings
+ * @s1: string 1
+ * @s2: string 2
+ * @n: byte number
+ *
+ * Return: pointer
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *ptr;
+	int num;
+	int i;
+	int j;
+	int len;
+
+	num = n;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	if (num < 0)
+		return (NULL);
+	if (num >= _strlen(s1))
+		num = _strlen(s2);
+
+	len = _strlen(s1) + num + 1;
+
+	ptr = malloc(sizeof(*ptr) * len);
+
+	if (ptr == NULL)
+		return (NULL);
+	for (i = 0; s1[i] != '\0'; i++)
+		ptr[i] = s1[i];
+
+	for (j = 0; s2[j] != '\0'; j++)
+		ptr[i + j] = s2[j];
+	ptr[i + j] = '\0';
+}
