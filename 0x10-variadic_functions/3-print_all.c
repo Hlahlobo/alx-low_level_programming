3d54c8b81cb1f550f0e169f9a539255d177ee14d
Hlahlobo <hfrakuoane@gmail.com>
2022-10-14 05:36:51 -0700

    3-print_all.c

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -13,7 +13,7 @@ void print_all(const char * const format, ...)
 {
 	va_list valist;
 	unsigned int i = 0, j, c = 0;
-
+	char *str;
 	const char t_arg[] = "cifs";
 
 	va_start(valist, format);
@@ -33,13 +33,13 @@ void print_all(const char * const format, ...)
 		switch (format[i])
 		{
 			case 'c':
-				printf("%c", va_arg(valist, int)), c=;
+				printf("%c", va_arg(valist, int)), c = 1;
 				break;
 			case 'i':
-				printf("%d", va_arg(valist, int)), c=;
+				printf("%d", va_arg(valist, int)), c = 1;
 				break;
 			case 'f':
-				printf("%f", va_arg(valist, int)), c=;
+				printf("%f", va_arg(valist, double)), c = 1;
 				break;
 			case 's':
 				if (!str)
@@ -48,7 +48,7 @@ void print_all(const char * const format, ...)
 					break;
 				}
 				printf("%s", str);
-		}i++;
+		} i++;
 	}
 	printf("\n"), va_end(valist);
 }
